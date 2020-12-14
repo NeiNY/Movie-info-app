405dcea0491c3e698bd7002e6b14e59450ae44f4
NeiNY <yfljghblevfnm@gmail.com>
2020-12-14 11:54:56 +0500

    HTML, SCSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">HOOKS</header>
+      <div className="App-main">
+        <form className="App-main-search">
+          <input type="text" placeholder="Search"></input>
+          <button>
+            <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false"><g>
+              <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+            </g></svg>
+          </button>
+        </form>
+        <div className="App-main-list">
+          <div className="App-main-list-content">
+            <div className="App-main-list-content-item">
+
+            </div>
+            <div className="App-main-list-content-item">
+
+            </div>
+            <div className="App-main-list-content-item">
+
+            </div>
+            <div className="App-main-list-content-item">
+
+            </div>
+            <div className="App-main-list-content-item">
+
+            </div>
+            <div className="App-main-list-content-item">
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className="App-footer">Made by Vlad Kryazhevskikh</footer>
+
     </div>
   );
 }
