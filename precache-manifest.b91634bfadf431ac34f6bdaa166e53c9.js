2c9e1f6e46bcb4734927d178df026ba949ac2385
NeiNY <yfljghblevfnm@gmail.com>
2021-03-12 16:39:44 +0500

    Updates

---
+++ precache-manifest.b91634bfadf431ac34f6bdaa166e53c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5de4b674d9d2c4a73048dd2ed3bf47",
+    "url": "/Movie-info-app/index.html"
+  },
+  {
+    "revision": "ed7b37a65f0493c7340c",
+    "url": "/Movie-info-app/static/css/main.823f6a59.chunk.css"
+  },
+  {
+    "revision": "cb71269abb565530dc3c",
+    "url": "/Movie-info-app/static/js/2.6768812d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-info-app/static/js/2.6768812d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7b37a65f0493c7340c",
+    "url": "/Movie-info-app/static/js/main.63d4f445.chunk.js"
+  },
+  {
+    "revision": "c19e6424c0e008966f52",
+    "url": "/Movie-info-app/static/js/runtime-main.4d24f4dc.js"
+  }
+]);
